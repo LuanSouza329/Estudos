e73f2581c0a5ac53c03a3376445c7fe3c4f919b0
LuanSouza329 <151081007+LuanSouza329@users.noreply.github.com>
2024-09-19 18:02:54 -0300

    "POO"

--- PHP/Aulas/classes/Nucleo/Mensagem.php
+++ PHP/Aulas/classes/Nucleo/Mensagem.php
@@ -1,5 +1,7 @@
 <?php 
 
+namespace PHP\Aulas\Classes\Nucleo; /*NameSpace da classe Mensagem pegando o seu diretório */
+
 class Mensagem {
 
     private $texto; /**Atributo com a sua visibilidade privada que somente será mudada através da Classe e seus métodos*/
@@ -11,12 +13,15 @@ class Mensagem {
     private function filtrar (string $mensagem) : string {
         return filter_var(value: trim(string: strip_tags(string: $mensagem)), filter: FILTER_SANITIZE_SPECIAL_CHARS);
     }
-    public function sucesso (string $mensagem) : Mensagem {
+    public function texto (string $mensagem) : Mensagem {
 
         $this->css = "alert alert-success";
 
         $this->texto = $this->filtrar(mensagem: $mensagem);
 
         return $this;
+    }
+    public function __tostring(): string{
+        return $this->renderizar();
     }
 }
--- PHP/Aulas/classes/index.php
+++ PHP/Aulas/classes/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Classes</title>
 </head>
 <body>
     
@@ -15,7 +15,10 @@
 
 include './Nucleo/Mensagem.php';
 
-$msg = new Mensagem ();
+use PHP\Aulas\Classes\Nucleo\Mensagem; /*Para usar clases que usam o nameSpace usamos a palavra reservada use, somente sendo chamada assim que iremos poder instanciar nosssas classes */
+
+// echo $msg->sucesso(mensagem: "Mensagem de sucesso")->renderizar(); /*Encadeamento de métodos, podemos chamar mais de um método de uma vez */
 
 
-echo $msg->sucesso(mensagem: "Mensagem de sucesso")->renderizar(); /*Encadeamento de métodos, podemos chamar mais de um método de uma vez */
+
+echo (new Mensagem())->texto(mensagem: "Olá mundo");
