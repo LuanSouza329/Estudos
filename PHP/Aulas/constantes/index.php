36d60abdacf00599ae005503e27ae3b8f2106f41
Luan <luansouza329@gmail.com>
2026-03-25 13:12:34 -0300

    Limpeza da pasta de PHP que não uso

--- PHP/Aulas/constantes/index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-define('NOME', 'LUAN DE SOUZA NUNES'); /*formas de definir constantes*/
-
-const IDADE = 31;
-
-
-echo NOME;
-echo IDADE;
--- PHP/Aulas/data/index.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-date_default_timezone_set(timezoneId: 'America/Sao_Paulo'); /*Fuso horário SP* - Este tipo de config deve ser deixado no arquivo de configuração do projeto*/
-
-$data = date (format: 'd-M-Y H:i:s');
-
-echo $data;
